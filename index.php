a2d5f6ee12ab0baf9c48427b38ba384a19b0bbf1
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-10 09:41:19 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 
 <?php
 
-$a = 1; 
+$x = 1;
 
 echo "I am ";
-echo $a;
+echo $x;
 echo "<br>";
-for ($a = 1; $a < 100; $a++;){
-$a +=1;
-}
-
-echo (" I added another one, I'm now... a = $a");
-
+  
+while($x <= 100) {   
+  echo "The number is: $x <br>";    
+  $x++;} 
+  
 ?>
+  
 </body>
 </html>
