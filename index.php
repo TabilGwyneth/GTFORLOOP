2c88d9dc28f02bc7b3bedc473636cde05ae091f1
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-10 09:52:57 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<?php
+
+$x = 1;
+
+echo "I am ";
+echo $x;
+echo "<br>";
+  
+while($x <= 100) {   
+  echo "I added another one, I'm now: $x <br>";    
+  $x++;
+} 
+  
+?>
+  
+</body>
+</html>
