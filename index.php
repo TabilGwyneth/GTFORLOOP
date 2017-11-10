f39a5f3770f6e2d89c9033a477367fd2ab75276a
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-10 10:02:41 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<?php
+
+$x = 1;
+
+  
+while($x <= 9) {   
+  $x = $x +1;    
+	echo ("I am now: $x");
+}
+
+if ($x == 9) {
+	echo "dammit I'm like a confused 6";
+
+} 
+
+
+  
+?>
+  
+</body>
+</html>
